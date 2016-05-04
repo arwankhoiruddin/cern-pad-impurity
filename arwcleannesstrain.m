8089a4be9c94870f253cdda397ed6779c678fa6e
Arwan Ahmad Khoiruddin <arwan.khoiruddin@gmail.com>
2016-05-04 14:51:01 +0700

    Added files to train and to run

---
+++ arwcleannesstrain.m
@@ -0,0 +1,80 @@
+close all
+clear
+clc
+
+Iorig = imread('chip01/row1/10x_11.jpg');
+
+I = rgb2gray(Iorig);
+tmpl = rgb2gray(imread('template/tmpl.jpg'));
+
+% resize the image
+scale = 0.2;
+Iorig = imresize(Iorig, scale);
+I = imresize(I, scale);
+tmpl = imresize(tmpl, scale);
+
+% calculate padding
+bx = size(I, 2);
+by = size(I, 1);
+tx = size(tmpl, 2); % used for bbox placement
+ty = size(tmpl, 1); 
+
+% fft
+Ga = fft2(I);
+Gb = fft2(tmpl, by, bx);
+c = real(ifft2((Ga.*conj(Gb)) ./ abs(Ga.*conj(Gb))));
+
+% find peak correlation
+[max_c, imax] = max(abs(c(:)));
+[ypeak, xpeak] = find(c==max(c(:)));
+% figure, surf(c), shading flat;
+
+position = [xpeak(1), ypeak(1), tx, ty];
+
+% crop the image
+for i=1:3
+    tmp = Iorig(:,:,i);
+    Icrop(:,:,i) = imcrop(tmp,position);
+end
+
+% change input into (3 x mn)
+input = zeros(size(Icrop,1)*size(Icrop,2), 3);
+for i=1:3
+    tmp = Icrop(:,:,i);
+    input(:,i) = double(tmp(:))/255;
+end
+
+options = [NaN 300 0.001 0];
+[centers, U, objFun] = fcm(input, 3, options);
+
+tmp = zeros(size(Icrop,1), size(Icrop, 2), 3);
+for i=1:3
+    tmp(:,:,i) = reshape(U(i,:), size(Icrop,1), size(Icrop,2));
+end
+
+% find the darkest point
+t = centers*255;
+t = 0.29*t(:,1) + 0.59*t(:,2) + 0.11*t(:,3);
+[~,cdarkest] = min(t);
+
+Iclust = zeros(size(Icrop,1), size(Icrop, 2));
+for i=1:size(Icrop,1)
+    for j=1:size(Icrop,2)
+        [~,loc] = max(tmp(i,j,:));
+        if loc==cdarkest
+            Iclust(i,j) = 1;
+        else
+            Iclust(i,j) = 0;
+        end
+    end
+end
+bw = im2bw(Iclust, 0.6);
+bw = bwareaopen(bw, 500);
+
+kotoran = Iclust - double(bw);
+
+figure, imshow(Icrop);
+figure, imshow(bw,[]);
+figure, imshow(kotoran,[]);
+
+save('trainresult.mat', 'centers');
---
+++ runcleanness.m
@@ -0,0 +1,81 @@
+close all
+clear
+clc
+
+Iorig = imread('chip01/row2/10x_03.jpg');
+
+I = rgb2gray(Iorig);
+tmpl = rgb2gray(imread('template/tmpl.jpg'));
+
+% resize the image
+scale = 0.2;
+Iorig = imresize(Iorig, scale);
+I = imresize(I, scale);
+tmpl = imresize(tmpl, scale);
+
+% calculate padding
+bx = size(I, 2);
+by = size(I, 1);
+tx = size(tmpl, 2); % used for bbox placement
+ty = size(tmpl, 1); 
+
+% fft
+Ga = fft2(I);
+Gb = fft2(tmpl, by, bx);
+c = real(ifft2((Ga.*conj(Gb)) ./ abs(Ga.*conj(Gb))));
+
+% find peak correlation
+[max_c, imax] = max(abs(c(:)));
+[ypeak, xpeak] = find(c==max(c(:)));
+% figure, surf(c), shading flat;
+
+position = [xpeak(1), ypeak(1), tx, ty];
+
+% crop the image
+for i=1:3
+    tmp = Iorig(:,:,i);
+    Icrop(:,:,i) = imcrop(tmp,position);
+end
+
+load('trainresult.mat');
+Igray = double((Icrop)) / 255;
+Iclust = zeros(size(Icrop,1), size(Icrop,2));
+for i=1:size(Icrop,1)
+    for j=1:size(Icrop,2)
+        % find euclidean distance of each pixel to each cluster
+        e = zeros(1, size(centers, 1));
+        for cluster=1:size(centers,1)
+            tmp = 0;
+            for color=1:3
+                tmp = tmp + (Igray(i,j,color) - centers(cluster,color))^2;
+            end
+            e(1, cluster) = sqrt(tmp);
+        end
+        
+        % find the darkest
+        t = 0.29*centers(:,1) + 0.59*centers(:,2) + 0.11*centers(:,3);
+        [~,cdarkest] = min(t);
+        
+        % plot
+        [~,cmember(i,j)] = max(e(:));
+        if cmember(i,j) == cdarkest
+            Iclust(i,j) = 0;
+        else
+            Iclust(i,j) = 1;
+        end
+    end
+end
+
+tr = size(Iclust,1)*size(Iclust,2) / 4;
+bw = bwareaopen(logical(Iclust), tr);
+
+noclean = Iclust - double(bw);
+
+if max(noclean(:))==0
+    disp('the pad is clean');
+else
+    disp('unclean area on the pad is detected');
+end
+
+figure, imshow(Icrop);
+figure, imshow(noclean,[]);
